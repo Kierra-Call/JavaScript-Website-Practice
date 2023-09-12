73ed35a95ac71d94362eec07c04fa49297b0076a
Kierra-Call <kierra.n.call@gmail.com>
2023-09-12 14:11:12 -0400

    update

--- likes/script.js
+++ likes/script.js
@@ -1,21 +1,20 @@
 var count1 = 9;
-function likes1() {
-    let countElement = document.querySelector("#like1");
-    count1++;
-    countElement.innerText = count1 + " like(s)"
-
-}
 var count2 = 12;
-function likes2() {
-    let countElement = document.querySelector("#like2");
-    count2++;
-    countElement.innerText = count2 + " like(s)"
-
-}
 var count3 = 9;
-function likes3() {
-    let countElement = document.querySelector("#like3");
-    count3++;
-    countElement.innerText = count3 + " like(s)"
-
+function addLike(element) {
+    if (element.id === "b1") {
+        let countElement = document.querySelector("#like1");
+        count1++;
+        countElement.innerText = count1 + " like(s)"
+    }
+    else if (element.id === "b2") {
+        let countElement = document.querySelector("#like2");
+        count2++;
+        countElement.innerText = count2 + " like(s)"
+    }
+    else {
+        let countElement = document.querySelector("#like3");
+        count3++;
+        countElement.innerText = count3 + " like(s)"
+    }
 }
