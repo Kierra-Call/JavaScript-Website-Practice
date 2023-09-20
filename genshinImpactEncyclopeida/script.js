0aa2650316379776c81940ef4bd4caa8cc7dd9b1
Kierra Call <142943885+Kierra-Call@users.noreply.github.com>
2023-09-20 17:45:47 -0400

    update

--- genshinImpactEncyclopeida/script.js
+++ genshinImpactEncyclopeida/script.js
@@ -65,15 +65,9 @@ function backgroundChange(vision) {
   }
 }
 function starChange(rarity) {
-  if (rarity === 5) {
-    var photo = document.querySelector("#stars");
-    photo.src = `./assets/otherImages/${rarity}star.png`;
-    photo.alt = "5 stars";
-  } else {
-    var photo = document.querySelector("#stars");
-    photo.src = `./assets/otherImages/${rarity}star.png`;
-    photo.alt = "4 stars";
-  }
+  let photo = document.querySelector("#stars");
+  photo.src = `./assets/otherImages/${rarity}star.png`;
+  photo.alt = `${rarity} stars`
 }
 
 // weapons 
@@ -87,5 +81,39 @@ async function getWeaponData(name) {
   var weaponType = document.querySelector("#weaponType");
   weaponType.innerText = weaponData.type;
   var weaponRarity = document.querySelector("#weaponRarity");
-  weaponRarity.innerText = weaponData.rarity;
+  weaponRarity.innerText = weaponData.rarity + " stars";
+  var weaponBaseAttk = document.querySelector("#weaponBaseAttk");
+  weaponBaseAttk.innerText = "Base Attack: " + weaponData.baseAttack;
+  var weaponSubStat = document.querySelector("#weaponSubStat");
+  weaponSubStat.innerText = weaponData.subStat;
+  var weaponPassName = document.querySelector("#weaponPassName");
+  weaponPassName.innerText = weaponData.passiveName;
+  var weaponPassDesc = document.querySelector("#weaponPassDesc");
+  weaponPassDesc.innerText = weaponData.passiveDesc;
+  var weaponAscMat = document.querySelector("#weaponAscMat");
+  weaponAscMat.innerText = "Weapon Ascension Material: " + weaponData.ascensionMaterial;
+
+  starChange2(weaponData.rarity);
+  imageChange2(name);
+  backgroundChange2(weaponData.rarity);
 }
+
+function starChange2(rarity) {
+  let photo = document.querySelector("#dynamicStar");
+  photo.src = `./assets/otherImages/${rarity}star.png`;
+  photo.alt = `${rarity} stars`
+  }
+
+  function imageChange2(name) {
+    var photo = document.querySelector("#dynamicWeapon");
+    photo.src = `./assets/weapons/${name}.png`;
+    photo.alt = name;
+  }
+
+  function backgroundChange2(rarity) {
+    var myElement = document.getElementById("dynamicStarBackdrop")
+    console.log(myElement);
+    myElement.classList.remove("star3","star1","star4","star5","star2");
+    myElement.classList.remove("standard");
+    myElement.classList.add("star" + rarity);
+  }
